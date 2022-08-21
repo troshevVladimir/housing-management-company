f8f6168e895b561cbe5942f5053fa03a1d76e7d5
Troshev Vladimir <v.troshev@3h.ru>
2022-08-21 20:01:07 +0300

    add router

---
+++ src/router/routes.js
@@ -0,0 +1,9 @@
+import MainPage from "@/views/MainPage.vue";
+import AboutPage from "@/views/AboutPage.vue";
+
+const routes = [
+  { path: "/", component: MainPage },
+  { path: "/about", component: AboutPage },
+];
+
+export default routes;
