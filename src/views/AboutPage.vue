7af4b669f79755c8dc9e87e4efb0c4e9f49f0838
Vladimir Troshev <troshevvladimir@gmail.com>
2022-09-09 00:14:02 +0300

    add map with coordinates

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,7 +1,45 @@
-<template>About page</template>
+<template>
+  <h1 class="houses-page__page-title">{{$route.params.title}}</h1>
+  <div id="map" style="width: 600px; height: 400px"></div>
+</template>
 
 <script>
-export default {};
+export default {
+	name: 'about',
+	mounted() {
+		this.ymap()
+	},
+	methods: {
+		ymap() {
+			ymaps.ready(init)
+			function init() {
+				var myMap = new ymaps.Map('map', {
+					center: [61.075447, 50.085236],
+					// from 0 (the entire world) to 19.
+					zoom: 17,
+				})
+
+				var placemark = new ymaps.Placemark(
+					[61.075447, 50.085236],
+					{
+						balloonContent: '<strong>Здание полу разрушено</strong>',
+						iconContent: 'Офис',
+					},
+					{
+						preset: 'islands#yellowStretchyIcon',
+						balloonCloseButton: true,
+					}
+				)
+				myMap.geoObjects.add(placemark)
+			}
+		},
+	},
+	computed: {
+		apiKey() {
+			return '79edb67c-16f5-4dca-8e2d-03e6aa4beebe'
+		},
+	},
+}
 </script>
 
 <style lang="scss"></style>
